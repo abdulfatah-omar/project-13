c5096b85ef0b5f2699ef4690e409cc86d3aac4b8
abdifatah-omar <abdifatahomar2023@gmail.com>
2023-10-27 19:31:32 -0500

    Write a message to yourself here

--- leetcode/app.js
+++ leetcode/app.js
@@ -1,1 +1,30 @@
 console.log("Hello world")
+
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+        
+        while (i < word1.length() && j < word2.length()) {
+            merged.append(word1.charAt(i));
+            merged.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        
+        // Append remaining characters from word1, if any
+        while (i < word1.length()) {
+            merged.append(word1.charAt(i));
+            i++;
+        }
+        
+        // Append remaining characters from word2, if any
+        while (j < word2.length()) {
+            merged.append(word2.charAt(j));
+            j++;
+        }
+        
+        return merged.toString();
+    }
+}
+
